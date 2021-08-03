yoge67855/git

status: serialize to path
Teach status serialization to take an optional pathname on
the command line to direct that cache data be written there
rather than to stdout.  When used this way, normal status
results will still be written to stdout.

When no path is given, only binary serialization data is
written to stdout.

Usage:
    git status --serialize[=<path>]

Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

t/t7524-serialized-status.sh
@@ -163,4 +163,27 @@ test_expect_success 'verify no-ahead-behind and serialized status integration' '
 	test_i18ncmp expect output
 '
 
+test_expect_success 'verify new --serialize=path mode' '
+	#test_when_finished "rm serialized_status.dat expect new_change.txt output.1 output.2" &&
+	cat >expect <<-\EOF &&
+	? expect
+	? output.1
+	? untracked/
+	? untracked_1.txt
+	EOF
+
+	git checkout -b serialize_path_branch master --track >/dev/null &&
+	touch alt_branch_changes.txt &&
+	git add alt_branch_changes.txt &&
+	test_tick &&
+	git commit -m"New commit on serialize_path_branch"  &&
+
+	git status --porcelain=v2 --serialize=serialized_status.dat >output.1 &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --deserialize=serialized_status.dat >output.2 &&
+	test_i18ncmp expect output.1 &&
+	test_i18ncmp expect output.2
+'
+
 test_done

ScriptBox99/microsoft-git

t/t7524-serialized-status.sh
@@ -170,4 +170,27 @@ test_expect_success 'verify no-ahead-behind and serialized status integration' '
 	test_cmp expect output
 '
 
+test_expect_success 'verify new --serialize=path mode' '
+	#test_when_finished "rm serialized_status.dat expect new_change.txt output.1 output.2" &&
+	cat >expect <<-\EOF &&
+	? expect
+	? output.1
+	? untracked/
+	? untracked_1.txt
+	EOF
+
+	git checkout -b serialize_path_branch main --track >/dev/null &&
+	touch alt_branch_changes.txt &&
+	git add alt_branch_changes.txt &&
+	test_tick &&
+	git commit -m"New commit on serialize_path_branch"  &&
+
+	git status --porcelain=v2 --serialize=serialized_status.dat >output.1 &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --deserialize=serialized_status.dat >output.2 &&
+	test_cmp expect output.1 &&
+	test_cmp expect output.2
+'
+
 test_done