yoge67855/git

Teach ahead-behind and serialized status to play nicely together

t/t7524-serialized-status.sh
@@ -137,4 +137,30 @@ test_expect_success 'verify serialized status handles path scopes' '
 	test_i18ncmp expect output
 '
 
+test_expect_success 'verify no-ahead-behind and serialized status integration' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-\EOF &&
+	# branch.oid 68d4a437ea4c2de65800f48c053d4d543b55c410
+	# branch.head alt_branch
+	# branch.upstream master
+	# branch.ab +1 -0
+	? expect
+	? serialized_status.dat
+	? untracked/
+	? untracked_1.txt
+	EOF
+
+	git checkout -b alt_branch master --track >/dev/null &&
+	touch alt_branch_changes.txt &&
+	git add alt_branch_changes.txt &&
+	test_tick &&
+	git commit -m"New commit on alt branch"  &&
+
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git -c status.aheadBehind=false status --porcelain=v2 --branch --ahead-behind --deserialize=serialized_status.dat >output &&
+	test_i18ncmp expect output
+'
+
 test_done

ScriptBox99/microsoft-git

t/t7524-serialized-status.sh
@@ -47,7 +47,13 @@ test_expect_success 'setup' '
 	git commit -m"Adding original file." &&
 	mkdir untracked &&
 	touch ignored.ign ignored_dir/ignored_2.txt \
-	      untracked_1.txt untracked/untracked_2.txt untracked/untracked_3.txt
+	      untracked_1.txt untracked/untracked_2.txt untracked/untracked_3.txt &&
+
+	test_oid_cache <<-EOF
+	branch_oid sha1:68d4a437ea4c2de65800f48c053d4d543b55c410
+
+	branch_oid sha256:6b95e4b1ea911dad213f2020840f5e92d3066cf9e38cf35f79412ec58d409ce4
+	EOF
 '
 
 test_expect_success 'verify untracked-files=complete with no conversion' '
@@ -135,6 +141,32 @@ test_expect_success 'verify serialized status handles path scopes' '
 	touch new_change.txt &&
 
 	git status --porcelain=v2 --deserialize=serialized_status.dat untracked >output &&
+	test_cmp expect output
+'
+
+test_expect_success 'verify no-ahead-behind and serialized status integration' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-EOF &&
+	# branch.oid $(test_oid branch_oid)
+	# branch.head alt_branch
+	# branch.upstream main
+	# branch.ab +1 -0
+	? expect
+	? serialized_status.dat
+	? untracked/
+	? untracked_1.txt
+	EOF
+
+	git checkout -b alt_branch main --track >/dev/null &&
+	touch alt_branch_changes.txt &&
+	git add alt_branch_changes.txt &&
+	test_tick &&
+	git commit -m"New commit on alt branch"  &&
+
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git -c status.aheadBehind=false status --porcelain=v2 --branch --ahead-behind --deserialize=serialized_status.dat >output &&
 	test_cmp expect output
 '
 