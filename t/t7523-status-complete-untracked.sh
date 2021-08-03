yoge67855/git

status: add status serialization mechanism
Teach STATUS to optionally serialize the results of a
status computation to a file.

Teach STATUS to optionally read an existing serialization
file and simply print the results, rather than actually
scanning.

This is intended for immediate status results on extremely
large repos and assumes the use of a service/daemon to
maintain a fresh current status snapshot.

Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

t/t7523-status-complete-untracked.sh
@@ -1,0 +1,39 @@
+#!/bin/sh
+
+test_description='git status untracked complete tests'
+
+. ./test-lib.sh
+
+test_expect_success 'setup' '
+	cat >.gitignore <<-\EOF &&
+	*.ign
+	ignored_dir/
+	EOF
+
+	mkdir tracked ignored_dir &&
+	touch tracked_1.txt tracked/tracked_1.txt &&
+	git add . &&
+	test_tick &&
+	git commit -m"Adding original file." &&
+	mkdir untracked &&
+	touch ignored.ign ignored_dir/ignored_2.txt \
+	      untracked_1.txt untracked/untracked_2.txt untracked/untracked_3.txt
+'
+
+test_expect_success 'verify untracked-files=complete' '
+	cat >expect <<-\EOF &&
+	? expect
+	? output
+	? untracked/
+	? untracked/untracked_2.txt
+	? untracked/untracked_3.txt
+	? untracked_1.txt
+	! ignored.ign
+	! ignored_dir/
+	EOF
+	
+	git status --porcelain=v2 --untracked-files=complete --ignored >output &&
+	test_i18ncmp expect output
+'
+
+test_done

t/t7524-serialized-status.sh
@@ -1,0 +1,140 @@
+#!/bin/sh
+
+test_description='git serialized status tests'
+
+. ./test-lib.sh
+
+# This file includes tests for serializing / deserializing
+# status data. These tests cover two basic features:
+#
+# [1] Because users can request different types of untracked-file
+#     and ignored file reporting, the cache data generated by
+#     serialize must use either the same untracked and ignored
+#     parameters as the later deserialize invocation; otherwise,
+#     the deserialize invocation must disregard the cached data
+#     and run a full scan itself.
+#
+#     To increase the number of cases where the cached status can
+#     be used, we have added a "--untracked-file=complete" option
+#     that reports a superset or union of the results from the
+#     "-u normal" and "-u all".  We combine this with a filter in
+#     deserialize to filter the results.
+#
+#     Ignored file reporting is simpler in that is an all or
+#     nothing; there are no subsets.
+#
+#     The tests here (in addition to confirming that a cache
+#     file can be generated and used by a subsequent status
+#     command) need to test this untracked-file filtering.
+#
+# [2] ensuring the status calls are using data from the status
+#     cache as expected.  This includes verifying cached data
+#     is used when appropriate as well as falling back to
+#     performing a new status scan when the data in the cache
+#     is insufficient/known stale.
+
+test_expect_success 'setup' '
+	cat >.gitignore <<-\EOF &&
+	*.ign
+	ignored_dir/
+	EOF
+
+	mkdir tracked ignored_dir &&
+	touch tracked_1.txt tracked/tracked_1.txt &&
+	git add . &&
+	test_tick &&
+	git commit -m"Adding original file." &&
+	mkdir untracked &&
+	touch ignored.ign ignored_dir/ignored_2.txt \
+	      untracked_1.txt untracked/untracked_2.txt untracked/untracked_3.txt
+'
+
+test_expect_success 'verify untracked-files=complete with no conversion' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-\EOF &&
+	? expect
+	? serialized_status.dat
+	? untracked/
+	? untracked/untracked_2.txt
+	? untracked/untracked_3.txt
+	? untracked_1.txt
+	! ignored.ign
+	! ignored_dir/
+	EOF
+	
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --untracked-files=complete --ignored=matching --deserialize=serialized_status.dat >output &&
+	test_i18ncmp expect output
+'
+
+test_expect_success 'verify untracked-files=complete to untracked-files=normal conversion' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-\EOF &&
+	? expect
+	? serialized_status.dat
+	? untracked/
+	? untracked_1.txt
+	EOF
+	
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --deserialize=serialized_status.dat >output &&
+	test_i18ncmp expect output
+'
+
+test_expect_success 'verify untracked-files=complete to untracked-files=all conversion' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-\EOF &&
+	? expect
+	? serialized_status.dat
+	? untracked/untracked_2.txt
+	? untracked/untracked_3.txt
+	? untracked_1.txt
+	! ignored.ign
+	! ignored_dir/
+	EOF
+	
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --untracked-files=all --ignored=matching --deserialize=serialized_status.dat >output &&
+	test_i18ncmp expect output
+'
+
+test_expect_success 'verify serialized status with non-convertible ignore mode does new scan' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-\EOF &&
+	? expect
+	? new_change.txt
+	? output
+	? serialized_status.dat
+	? untracked/
+	? untracked_1.txt
+	! ignored.ign
+	! ignored_dir/
+	EOF
+	
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --ignored --deserialize=serialized_status.dat >output &&
+	test_i18ncmp expect output
+'
+
+test_expect_success 'verify serialized status handles path scopes' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-\EOF &&
+	? untracked/
+	EOF
+	
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --deserialize=serialized_status.dat untracked >output &&
+	test_i18ncmp expect output
+'
+
+test_done

wt-status-serialize.c
@@ -1,0 +1,214 @@
+#include "cache.h"
+#include "wt-status.h"
+#include "pkt-line.h"
+
+static struct trace_key trace_serialize = TRACE_KEY_INIT(SERIALIZE);
+
+/*
+ * Write V1 header fields.
+ */
+static void wt_serialize_v1_header(struct wt_status *s, int fd)
+{
+	/*
+	 * Write select fields from the current index to help
+	 * the deserializer recognize a stale data set.
+	 */
+	packet_write_fmt(fd, "index_mtime %d %d\n",
+			 the_index.timestamp.sec,
+			 the_index.timestamp.nsec);
+
+	/*
+	 * Write data from wt_status to qualify this status report.
+	 * That is, if this run specified "-uno", the consumer of
+	 * our serialization should know that.
+	 */
+	packet_write_fmt(fd, "is_initial %d\n", s->is_initial);
+	if (s->branch)
+		packet_write_fmt(fd, "branch %s\n", s->branch);
+	if (s->reference)
+		packet_write_fmt(fd, "reference %s\n", s->reference);
+	/* pathspec */
+	/* verbose */
+	/* amend */
+	packet_write_fmt(fd, "whence %d\n", s->whence);
+	/* nowarn */
+	/* use_color */
+	/* no_gettext */
+	/* display_comment_prefix */
+	/* relative_paths */
+	/* submodule_summary */
+	packet_write_fmt(fd, "show_ignored_mode %d\n", s->show_ignored_mode);
+	packet_write_fmt(fd, "show_untracked_files %d\n", s->show_untracked_files);
+	if (s->ignore_submodule_arg)
+		packet_write_fmt(fd, "ignore_submodule_arg %s\n", s->ignore_submodule_arg);
+	/* color_palette */
+	/* colopts */
+	/* null_termination */
+	/* commit_template */
+	/* show_branch */
+	/* show_stash */
+	packet_write_fmt(fd, "hints %d\n", s->hints);
+	packet_write_fmt(fd, "detect_rename %d\n", s->detect_rename);
+	packet_write_fmt(fd, "rename_score %d\n", s->rename_score);
+	packet_write_fmt(fd, "rename_limit %d\n", s->rename_limit);
+	packet_write_fmt(fd, "detect_break %d\n", s->detect_break);
+	/* status_format */
+	packet_write_fmt(fd, "sha1_commit %s\n", sha1_to_hex(s->sha1_commit));
+	packet_write_fmt(fd, "commitable %d\n", s->commitable);
+	packet_write_fmt(fd, "workdir_dirty %d\n", s->workdir_dirty);
+	/* prefix */
+	packet_flush(fd);
+}
+
+/*
+ * Print changed/unmerged items.
+ * We write raw (not c-quoted) pathname(s).  The head_path is only
+ * set when status computed a rename/copy.
+ *
+ * We ALWAYS write a final LF to the packet-line (for debugging)
+ * even though Linux pathnames allow LFs.
+ */
+static inline void wt_serialize_v1_changed(struct wt_status *s, int fd,
+					   struct string_list_item *item)
+{
+	struct wt_status_change_data *d = item->util;
+	struct wt_status_serialize_data sd;
+	char *begin;
+	char *end;
+	char *p;
+	int len_path, len_head_path;
+
+	trace_printf_key(&trace_serialize,
+		"change: %d %d %d %d %o %o %o %d %d %s %s '%s' '%s'",
+		d->worktree_status,
+		d->index_status,
+		d->stagemask,
+		d->score,
+		d->mode_head,
+		d->mode_index,
+		d->mode_worktree,
+		d->dirty_submodule,
+		d->new_submodule_commits,
+		oid_to_hex(&d->oid_head),
+		oid_to_hex(&d->oid_index),
+		item->string,
+		(d->head_path ? d->head_path : ""));
+
+	sd.fixed.worktree_status       = htonl(d->worktree_status);
+	sd.fixed.index_status          = htonl(d->index_status);
+	sd.fixed.stagemask             = htonl(d->stagemask);
+	sd.fixed.score                 = htonl(d->score);
+	sd.fixed.mode_head             = htonl(d->mode_head);
+	sd.fixed.mode_index            = htonl(d->mode_index);
+	sd.fixed.mode_worktree         = htonl(d->mode_worktree);
+	sd.fixed.dirty_submodule       = htonl(d->dirty_submodule);
+	sd.fixed.new_submodule_commits = htonl(d->new_submodule_commits);
+	oidcpy(&sd.fixed.oid_head,  &d->oid_head);
+	oidcpy(&sd.fixed.oid_index, &d->oid_index);
+
+	begin = (char *)&sd;
+	end = begin + sizeof(sd);
+
+	p = sd.variant;
+
+	/*
+	 * Write <path> NUL [<head_path>] NUL LF at the end of the buffer.
+	 */
+	len_path = strlen(item->string);
+	len_head_path = d->head_path ? strlen(d->head_path) : 0;
+
+	/*
+	 * This is a bit of a hack, but I don't want to split the
+	 * status detail record across multiple pkt-lines.
+	 */
+	if (p + len_path + 1 + len_head_path + 1 + 1 >= end)
+		BUG("path to long to serialize '%s'", item->string);
+
+	memcpy(p, item->string, len_path);
+	p += len_path;
+	*p++ = '\0';
+
+	if (len_head_path) {
+		memcpy(p, d->head_path, len_head_path);
+		p += len_head_path;
+	}
+	*p++ = '\0';
+	*p++ = '\n';
+
+	if (packet_write_gently(fd, begin, (p - begin)))
+		BUG("cannot serialize '%s'", item->string);
+}
+
+/*
+ * Write raw (not c-quoted) pathname for an untracked item.
+ * We ALWAYS write a final LF to the packet-line (for debugging)
+ * even though Linux pathnames allows LFs.  That is, deserialization
+ * should use the packet-line length and omit the final LF.
+ */
+static inline void wt_serialize_v1_untracked(struct wt_status *s, int fd,
+					     struct string_list_item *item)
+{
+	packet_write_fmt(fd, "%s\n", item->string);
+}
+
+/*
+ * Write raw (not c-quoted) pathname for an ignored item.
+ * We ALWAYS write a final LF to the packet-line (for debugging)
+ * even though Linux pathnames allows LFs.
+ */
+static inline void wt_serialize_v1_ignored(struct wt_status *s, int fd,
+					   struct string_list_item *item)
+{
+	packet_write_fmt(fd, "%s\n", item->string);
+}
+
+/*
+ * Serialize the list of changes to stdout.  The goal of this
+ * is to just serialize the key fields in wt_status so that a
+ * later command can rebuilt it and do the printing.
+ *
+ * We DO NOT include the contents of wt_status_state NOR
+ * current branch info.  This info easily gets stale and
+ * is relatively quick for the status consumer to compute
+ * as necessary.
+ */
+void wt_status_serialize_v1(struct wt_status *s)
+{
+	int fd = 1; /* we always write to stdout */
+	struct string_list_item *iter;
+	int k;
+
+	/*
+	 * version header must be first line.
+	 */
+	packet_write_fmt(fd, "version 1\n");
+
+	wt_serialize_v1_header(s, fd);
+
+	if (s->change.nr > 0) {
+		packet_write_fmt(fd, "changed %d\n", s->change.nr);
+		for (k = 0; k < s->change.nr; k++) {
+			iter = &(s->change.items[k]);
+			wt_serialize_v1_changed(s, fd, iter);
+		}
+		packet_flush(fd);
+	}
+
+	if (s->untracked.nr > 0) {
+		packet_write_fmt(fd, "untracked %d\n", s->untracked.nr);
+		for (k = 0; k < s->untracked.nr; k++) {
+			iter = &(s->untracked.items[k]);
+			wt_serialize_v1_untracked(s, fd, iter);
+		}
+		packet_flush(fd);
+	}
+
+	if (s->ignored.nr > 0) {
+		packet_write_fmt(fd, "ignored %d\n", s->ignored.nr);
+		for (k = 0; k < s->ignored.nr; k++) {
+			iter = &(s->ignored.items[k]);
+			wt_serialize_v1_ignored(s, fd, iter);
+		}
+		packet_flush(fd);
+	}
+}

wt-status.h
@@ -5,6 +5,7 @@
 #include "string-list.h"
 #include "color.h"
 #include "pathspec.h"
+#include "pkt-line.h"
 #include "remote.h"
 
 struct worktree;
@@ -25,7 +26,8 @@ enum color_wt_status {
 enum untracked_status_type {
 	SHOW_NO_UNTRACKED_FILES,
 	SHOW_NORMAL_UNTRACKED_FILES,
-	SHOW_ALL_UNTRACKED_FILES
+	SHOW_ALL_UNTRACKED_FILES,
+	SHOW_COMPLETE_UNTRACKED_FILES,
 };
 
 enum show_ignored_type {
@@ -59,6 +61,7 @@ enum wt_status_format {
 	STATUS_FORMAT_SHORT,
 	STATUS_FORMAT_PORCELAIN,
 	STATUS_FORMAT_PORCELAIN_V2,
+	STATUS_FORMAT_SERIALIZE_V1,
 
 	STATUS_FORMAT_UNSPECIFIED
 };
@@ -148,5 +151,52 @@ int has_unstaged_changes(int ignore_submodules);
 int has_uncommitted_changes(int ignore_submodules);
 int require_clean_work_tree(const char *action, const char *hint,
 	int ignore_submodules, int gently);
+
+#define DESERIALIZE_OK  0
+#define DESERIALIZE_ERR 1
+
+struct wt_status_serialize_data_fixed
+{
+	uint32_t worktree_status;
+	uint32_t index_status;
+	uint32_t stagemask;
+	uint32_t score;
+	uint32_t mode_head;
+	uint32_t mode_index;
+	uint32_t mode_worktree;
+	uint32_t dirty_submodule;
+	uint32_t new_submodule_commits;
+	struct object_id oid_head;
+	struct object_id oid_index;
+};
+
+/*
+ * Consume the maximum amount of data possible in a
+ * packet-line record.  This is overkill because we
+ * have at most 2 relative pathnames, but means we
+ * don't need to allocate a variable length structure.
+ */
+struct wt_status_serialize_data
+{
+	struct wt_status_serialize_data_fixed fixed;
+	char variant[LARGE_PACKET_DATA_MAX
+		     - sizeof(struct wt_status_serialize_data_fixed)];
+};
+
+/*
+ * Serialize computed status scan results using "version 1" format
+ * to the given file.
+ */
+void wt_status_serialize_v1(struct wt_status *s);
+
+/*
+ * Deserialize existing status results from the given file and
+ * populate a (new) "struct wt_status".  Use the contents of "cmd_s"
+ * (computed from the command line arguments) to verify that the
+ * cached data is compatible and overlay various display-related
+ * fields.
+ */
+int wt_status_deserialize(const struct wt_status *cmd_s,
+			  const char *path);
 
 #endif /* STATUS_H */

ScriptBox99/microsoft-git

t/t7523-status-complete-untracked.sh
@@ -1,0 +1,39 @@
+#!/bin/sh
+
+test_description='git status untracked complete tests'
+
+. ./test-lib.sh
+
+test_expect_success 'setup' '
+	cat >.gitignore <<-\EOF &&
+	*.ign
+	ignored_dir/
+	EOF
+
+	mkdir tracked ignored_dir &&
+	touch tracked_1.txt tracked/tracked_1.txt &&
+	git add . &&
+	test_tick &&
+	git commit -m"Adding original file." &&
+	mkdir untracked &&
+	touch ignored.ign ignored_dir/ignored_2.txt \
+	      untracked_1.txt untracked/untracked_2.txt untracked/untracked_3.txt
+'
+
+test_expect_success 'verify untracked-files=complete' '
+	cat >expect <<-\EOF &&
+	? expect
+	? output
+	? untracked/
+	? untracked/untracked_2.txt
+	? untracked/untracked_3.txt
+	? untracked_1.txt
+	! ignored.ign
+	! ignored_dir/
+	EOF
+
+	git status --porcelain=v2 --untracked-files=complete --ignored >output &&
+	test_cmp expect output
+'
+
+test_done

t/t7524-serialized-status.sh
@@ -1,0 +1,141 @@
+#!/bin/sh
+
+test_description='git serialized status tests'
+
+. ./test-lib.sh
+
+# This file includes tests for serializing / deserializing
+# status data. These tests cover two basic features:
+#
+# [1] Because users can request different types of untracked-file
+#     and ignored file reporting, the cache data generated by
+#     serialize must use either the same untracked and ignored
+#     parameters as the later deserialize invocation; otherwise,
+#     the deserialize invocation must disregard the cached data
+#     and run a full scan itself.
+#
+#     To increase the number of cases where the cached status can
+#     be used, we have added a "--untracked-file=complete" option
+#     that reports a superset or union of the results from the
+#     "-u normal" and "-u all".  We combine this with a filter in
+#     deserialize to filter the results.
+#
+#     Ignored file reporting is simpler in that is an all or
+#     nothing; there are no subsets.
+#
+#     The tests here (in addition to confirming that a cache
+#     file can be generated and used by a subsequent status
+#     command) need to test this untracked-file filtering.
+#
+# [2] ensuring the status calls are using data from the status
+#     cache as expected.  This includes verifying cached data
+#     is used when appropriate as well as falling back to
+#     performing a new status scan when the data in the cache
+#     is insufficient/known stale.
+
+test_expect_success 'setup' '
+	git branch -M main &&
+	cat >.gitignore <<-\EOF &&
+	*.ign
+	ignored_dir/
+	EOF
+
+	mkdir tracked ignored_dir &&
+	touch tracked_1.txt tracked/tracked_1.txt &&
+	git add . &&
+	test_tick &&
+	git commit -m"Adding original file." &&
+	mkdir untracked &&
+	touch ignored.ign ignored_dir/ignored_2.txt \
+	      untracked_1.txt untracked/untracked_2.txt untracked/untracked_3.txt
+'
+
+test_expect_success 'verify untracked-files=complete with no conversion' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-\EOF &&
+	? expect
+	? serialized_status.dat
+	? untracked/
+	? untracked/untracked_2.txt
+	? untracked/untracked_3.txt
+	? untracked_1.txt
+	! ignored.ign
+	! ignored_dir/
+	EOF
+
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --untracked-files=complete --ignored=matching --deserialize=serialized_status.dat >output &&
+	test_cmp expect output
+'
+
+test_expect_success 'verify untracked-files=complete to untracked-files=normal conversion' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-\EOF &&
+	? expect
+	? serialized_status.dat
+	? untracked/
+	? untracked_1.txt
+	EOF
+
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --deserialize=serialized_status.dat >output &&
+	test_cmp expect output
+'
+
+test_expect_success 'verify untracked-files=complete to untracked-files=all conversion' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-\EOF &&
+	? expect
+	? serialized_status.dat
+	? untracked/untracked_2.txt
+	? untracked/untracked_3.txt
+	? untracked_1.txt
+	! ignored.ign
+	! ignored_dir/
+	EOF
+
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --untracked-files=all --ignored=matching --deserialize=serialized_status.dat >output &&
+	test_cmp expect output
+'
+
+test_expect_success 'verify serialized status with non-convertible ignore mode does new scan' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-\EOF &&
+	? expect
+	? new_change.txt
+	? output
+	? serialized_status.dat
+	? untracked/
+	? untracked_1.txt
+	! ignored.ign
+	! ignored_dir/
+	EOF
+
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --ignored --deserialize=serialized_status.dat >output &&
+	test_cmp expect output
+'
+
+test_expect_success 'verify serialized status handles path scopes' '
+	test_when_finished "rm serialized_status.dat new_change.txt output" &&
+	cat >expect <<-\EOF &&
+	? untracked/
+	EOF
+
+	git status --untracked-files=complete --ignored=matching --serialize >serialized_status.dat &&
+	touch new_change.txt &&
+
+	git status --porcelain=v2 --deserialize=serialized_status.dat untracked >output &&
+	test_cmp expect output
+'
+
+test_done

wt-status-serialize.c
@@ -1,0 +1,213 @@
+#include "cache.h"
+#include "wt-status.h"
+#include "pkt-line.h"
+
+static struct trace_key trace_serialize = TRACE_KEY_INIT(SERIALIZE);
+
+/*
+ * Write V1 header fields.
+ */
+static void wt_serialize_v1_header(struct wt_status *s, int fd)
+{
+	/*
+	 * Write select fields from the current index to help
+	 * the deserializer recognize a stale data set.
+	 */
+	packet_write_fmt(fd, "index_mtime %d %d\n",
+			 s->repo->index->timestamp.sec,
+			 s->repo->index->timestamp.nsec);
+
+	/*
+	 * Write data from wt_status to qualify this status report.
+	 * That is, if this run specified "-uno", the consumer of
+	 * our serialization should know that.
+	 */
+	packet_write_fmt(fd, "is_initial %d\n", s->is_initial);
+	if (s->branch)
+		packet_write_fmt(fd, "branch %s\n", s->branch);
+	if (s->reference)
+		packet_write_fmt(fd, "reference %s\n", s->reference);
+	/* pathspec */
+	/* verbose */
+	/* amend */
+	packet_write_fmt(fd, "whence %d\n", s->whence);
+	/* nowarn */
+	/* use_color */
+	/* no_gettext */
+	/* display_comment_prefix */
+	/* relative_paths */
+	/* submodule_summary */
+	packet_write_fmt(fd, "show_ignored_mode %d\n", s->show_ignored_mode);
+	packet_write_fmt(fd, "show_untracked_files %d\n", s->show_untracked_files);
+	if (s->ignore_submodule_arg)
+		packet_write_fmt(fd, "ignore_submodule_arg %s\n", s->ignore_submodule_arg);
+	/* color_palette */
+	/* colopts */
+	/* null_termination */
+	/* commit_template */
+	/* show_branch */
+	/* show_stash */
+	packet_write_fmt(fd, "hints %d\n", s->hints);
+	packet_write_fmt(fd, "detect_rename %d\n", s->detect_rename);
+	packet_write_fmt(fd, "rename_score %d\n", s->rename_score);
+	packet_write_fmt(fd, "rename_limit %d\n", s->rename_limit);
+	/* status_format */
+	packet_write_fmt(fd, "sha1_commit %s\n", oid_to_hex(&s->oid_commit));
+	packet_write_fmt(fd, "committable %d\n", s->committable);
+	packet_write_fmt(fd, "workdir_dirty %d\n", s->workdir_dirty);
+	/* prefix */
+	packet_flush(fd);
+}
+
+/*
+ * Print changed/unmerged items.
+ * We write raw (not c-quoted) pathname(s).  The rename_source is only
+ * set when status computed a rename/copy.
+ *
+ * We ALWAYS write a final LF to the packet-line (for debugging)
+ * even though Linux pathnames allow LFs.
+ */
+static inline void wt_serialize_v1_changed(struct wt_status *s, int fd,
+					   struct string_list_item *item)
+{
+	struct wt_status_change_data *d = item->util;
+	struct wt_status_serialize_data sd;
+	char *begin;
+	char *end;
+	char *p;
+	int len_path, len_rename_source;
+
+	trace_printf_key(&trace_serialize,
+		"change: %d %d %d %d %o %o %o %d %d %s %s '%s' '%s'",
+		d->worktree_status,
+		d->index_status,
+		d->stagemask,
+		d->rename_score,
+		d->mode_head,
+		d->mode_index,
+		d->mode_worktree,
+		d->dirty_submodule,
+		d->new_submodule_commits,
+		oid_to_hex(&d->oid_head),
+		oid_to_hex(&d->oid_index),
+		item->string,
+		(d->rename_source ? d->rename_source : ""));
+
+	sd.fixed.worktree_status       = htonl(d->worktree_status);
+	sd.fixed.index_status          = htonl(d->index_status);
+	sd.fixed.stagemask             = htonl(d->stagemask);
+	sd.fixed.rename_score          = htonl(d->rename_score);
+	sd.fixed.mode_head             = htonl(d->mode_head);
+	sd.fixed.mode_index            = htonl(d->mode_index);
+	sd.fixed.mode_worktree         = htonl(d->mode_worktree);
+	sd.fixed.dirty_submodule       = htonl(d->dirty_submodule);
+	sd.fixed.new_submodule_commits = htonl(d->new_submodule_commits);
+	oidcpy(&sd.fixed.oid_head,  &d->oid_head);
+	oidcpy(&sd.fixed.oid_index, &d->oid_index);
+
+	begin = (char *)&sd;
+	end = begin + sizeof(sd);
+
+	p = sd.variant;
+
+	/*
+	 * Write <path> NUL [<rename_source>] NUL LF at the end of the buffer.
+	 */
+	len_path = strlen(item->string);
+	len_rename_source = d->rename_source ? strlen(d->rename_source) : 0;
+
+	/*
+	 * This is a bit of a hack, but I don't want to split the
+	 * status detail record across multiple pkt-lines.
+	 */
+	if (p + len_path + 1 + len_rename_source + 1 + 1 >= end)
+		BUG("path to long to serialize '%s'", item->string);
+
+	memcpy(p, item->string, len_path);
+	p += len_path;
+	*p++ = '\0';
+
+	if (len_rename_source) {
+		memcpy(p, d->rename_source, len_rename_source);
+		p += len_rename_source;
+	}
+	*p++ = '\0';
+	*p++ = '\n';
+
+	if (packet_write_gently(fd, begin, (p - begin)))
+		BUG("cannot serialize '%s'", item->string);
+}
+
+/*
+ * Write raw (not c-quoted) pathname for an untracked item.
+ * We ALWAYS write a final LF to the packet-line (for debugging)
+ * even though Linux pathnames allows LFs.  That is, deserialization
+ * should use the packet-line length and omit the final LF.
+ */
+static inline void wt_serialize_v1_untracked(struct wt_status *s, int fd,
+					     struct string_list_item *item)
+{
+	packet_write_fmt(fd, "%s\n", item->string);
+}
+
+/*
+ * Write raw (not c-quoted) pathname for an ignored item.
+ * We ALWAYS write a final LF to the packet-line (for debugging)
+ * even though Linux pathnames allows LFs.
+ */
+static inline void wt_serialize_v1_ignored(struct wt_status *s, int fd,
+					   struct string_list_item *item)
+{
+	packet_write_fmt(fd, "%s\n", item->string);
+}
+
+/*
+ * Serialize the list of changes to stdout.  The goal of this
+ * is to just serialize the key fields in wt_status so that a
+ * later command can rebuilt it and do the printing.
+ *
+ * We DO NOT include the contents of wt_status_state NOR
+ * current branch info.  This info easily gets stale and
+ * is relatively quick for the status consumer to compute
+ * as necessary.
+ */
+void wt_status_serialize_v1(struct wt_status *s)
+{
+	int fd = 1; /* we always write to stdout */
+	struct string_list_item *iter;
+	int k;
+
+	/*
+	 * version header must be first line.
+	 */
+	packet_write_fmt(fd, "version 1\n");
+
+	wt_serialize_v1_header(s, fd);
+
+	if (s->change.nr > 0) {
+		packet_write_fmt(fd, "changed %d\n", s->change.nr);
+		for (k = 0; k < s->change.nr; k++) {
+			iter = &(s->change.items[k]);
+			wt_serialize_v1_changed(s, fd, iter);
+		}
+		packet_flush(fd);
+	}
+
+	if (s->untracked.nr > 0) {
+		packet_write_fmt(fd, "untracked %d\n", s->untracked.nr);
+		for (k = 0; k < s->untracked.nr; k++) {
+			iter = &(s->untracked.items[k]);
+			wt_serialize_v1_untracked(s, fd, iter);
+		}
+		packet_flush(fd);
+	}
+
+	if (s->ignored.nr > 0) {
+		packet_write_fmt(fd, "ignored %d\n", s->ignored.nr);
+		for (k = 0; k < s->ignored.nr; k++) {
+			iter = &(s->ignored.items[k]);
+			wt_serialize_v1_ignored(s, fd, iter);
+		}
+		packet_flush(fd);
+	}
+}

wt-status.h
@@ -4,6 +4,7 @@
 #include "string-list.h"
 #include "color.h"
 #include "pathspec.h"
+#include "pkt-line.h"
 #include "remote.h"
 
 struct repository;
@@ -25,7 +26,8 @@ enum color_wt_status {
 enum untracked_status_type {
 	SHOW_NO_UNTRACKED_FILES,
 	SHOW_NORMAL_UNTRACKED_FILES,
-	SHOW_ALL_UNTRACKED_FILES
+	SHOW_ALL_UNTRACKED_FILES,
+	SHOW_COMPLETE_UNTRACKED_FILES,
 };
 
 enum show_ignored_type {
@@ -73,6 +75,7 @@ enum wt_status_format {
 	STATUS_FORMAT_SHORT,
 	STATUS_FORMAT_PORCELAIN,
 	STATUS_FORMAT_PORCELAIN_V2,
+	STATUS_FORMAT_SERIALIZE_V1,
 
 	STATUS_FORMAT_UNSPECIFIED
 };
@@ -181,5 +184,52 @@ int require_clean_work_tree(struct repository *repo,
 			    const char *hint,
 			    int ignore_submodules,
 			    int gently);
+
+#define DESERIALIZE_OK  0
+#define DESERIALIZE_ERR 1
+
+struct wt_status_serialize_data_fixed
+{
+	uint32_t worktree_status;
+	uint32_t index_status;
+	uint32_t stagemask;
+	uint32_t rename_score;
+	uint32_t mode_head;
+	uint32_t mode_index;
+	uint32_t mode_worktree;
+	uint32_t dirty_submodule;
+	uint32_t new_submodule_commits;
+	struct object_id oid_head;
+	struct object_id oid_index;
+};
+
+/*
+ * Consume the maximum amount of data possible in a
+ * packet-line record.  This is overkill because we
+ * have at most 2 relative pathnames, but means we
+ * don't need to allocate a variable length structure.
+ */
+struct wt_status_serialize_data
+{
+	struct wt_status_serialize_data_fixed fixed;
+	char variant[LARGE_PACKET_DATA_MAX
+		     - sizeof(struct wt_status_serialize_data_fixed)];
+};
+
+/*
+ * Serialize computed status scan results using "version 1" format
+ * to the given file.
+ */
+void wt_status_serialize_v1(struct wt_status *s);
+
+/*
+ * Deserialize existing status results from the given file and
+ * populate a (new) "struct wt_status".  Use the contents of "cmd_s"
+ * (computed from the command line arguments) to verify that the
+ * cached data is compatible and overlay various display-related
+ * fields.
+ */
+int wt_status_deserialize(const struct wt_status *cmd_s,
+			  const char *path);
 
 #endif /* STATUS_H */