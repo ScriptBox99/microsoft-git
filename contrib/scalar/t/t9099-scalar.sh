vdye/git

scalar unregister: handle deleted enlistment directory gracefully
When a user deleted an enlistment manually, let's be generous and
_still_ unregister it.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

contrib/scalar/t/t9099-scalar.sh
@@ -154,4 +154,19 @@ test_expect_success '`scalar clone` with GVFS-enabled server' '
 	)
 '
 
+test_expect_success 'scalar unregister' '
+	git init poof/src &&
+	scalar register poof/src &&
+	git config --get --global --fixed-value \
+		maintenance.repo "$(pwd)/poof/src" &&
+	scalar list >scalar.repos &&
+	grep -F "$(pwd)/poof/src" scalar.repos &&
+	rm -rf poof/src/.git &&
+	scalar unregister poof &&
+	test_must_fail git config --get --global --fixed-value \
+		maintenance.repo "$(pwd)/poof/src" &&
+	scalar list >scalar.repos &&
+	! grep -F "$(pwd)/poof/src" scalar.repos
+'
+
 test_done

ScriptBox99/microsoft-git

contrib/scalar/t/t9099-scalar.sh
@@ -14,4 +14,19 @@ test_expect_success 'scalar shows a usage' '
 	test_expect_code 129 scalar -h
 '
 
+test_expect_success 'scalar unregister' '
+	git init vanish/src &&
+	scalar register vanish/src &&
+	git config --get --global --fixed-value \
+		maintenance.repo "$(pwd)/vanish/src" &&
+	scalar list >scalar.repos &&
+	grep -F "$(pwd)/vanish/src" scalar.repos &&
+	rm -rf vanish/src/.git &&
+	scalar unregister vanish &&
+	test_must_fail git config --get --global --fixed-value \
+		maintenance.repo "$(pwd)/vanish/src" &&
+	scalar list >scalar.repos &&
+	! grep -F "$(pwd)/vanish/src" scalar.repos
+'
+
 test_done