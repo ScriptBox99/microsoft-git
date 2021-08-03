yoge67855/git

sha1-file: create shared-cache directory if it doesn't exist
The config variable `gvfs.sharedCache` contains the pathname to an alternate
<odb> that will be used by `gvfs-helper` to store dynamically-fetched missing
objects.  If this directory does not exist on disk, `prepare_alt_odb()` omits
this directory from the in-memory list of alternates.  This causes `git`
commands (and `gvfs-helper` in particular) to fall-back to `.git/objects` for
storage of these objects.  This disables the shared-cache and leads to poorer
performance.

Teach `alt_obj_usable()` and `prepare_alt_odb()`, match up the directory
named in `gvfs.sharedCache` with an entry in `.git/objects/info/alternates`
and force-create the `<odb>` root directory (and the associated `<odb>/pack`
directory) if necessary.

If the value of `gvfs.sharedCache` refers to a directory that is NOT listed
as an alternate, create an in-memory alternate entry in the odb-list.  (This
is similar to how GIT_ALTERNATE_OBJECT_DIRECTORIES works.)

This work happens the first time that `prepare_alt_odb()` is called.

Furthermore, teach the `--shared-cache=<odb>` command line option in
`gvfs-helper` (which is runs after the first call to `prepare_alt_odb()`)
to override the inherited shared-cache (and again, create the ODB directory
if necessary).

Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

gvfs-helper-client.c
@@ -224,6 +224,13 @@ static int ghc__get__receive_response(struct child_process *process,
 	return err;
 }
 
+/*
+ * Select the preferred ODB for fetching missing objects.
+ * This should be the alternate with the same directory
+ * name as set in `gvfs.sharedCache`.
+ *
+ * Fallback to .git/objects if necessary.
+ */
 static void ghc__choose_odb(void)
 {
 	struct object_directory *odb;
@@ -231,22 +238,19 @@ static void ghc__choose_odb(void)
 	if (ghs__chosen_odb)
 		return;
 
+	ghs__chosen_odb = the_repository->objects->odb;
+
 	prepare_alt_odb(the_repository);
 
-	if (gvfs_shared_cache_pathname && *gvfs_shared_cache_pathname) {
-		for (odb = the_repository->objects->odb; odb; odb = odb->next) {
-			if (!strcmp(odb->path, gvfs_shared_cache_pathname)) {
-				ghs__chosen_odb = odb;
-				return;
-			}
+	if (!gvfs_shared_cache_pathname.len)
+		return;
+
+	for (odb = the_repository->objects->odb->next; odb; odb = odb->next) {
+		if (!strcmp(odb->path, gvfs_shared_cache_pathname.buf)) {
+			ghs__chosen_odb = odb;
+			return;
 		}
 	}
-
-	/*
-	 * Use .git/objects if "gvfs.sharedcache" not set or set to an
-	 * unknown pathname.
-	 */
-	ghs__chosen_odb = the_repository->objects->odb;
 }
 
 static int ghc__get(enum ghc__created *p_ghc)

ScriptBox99/microsoft-git

gvfs-helper-client.c
@@ -205,13 +205,32 @@ static int gh_client__get__receive_response(
 	return err;
 }
 
+/*
+ * Select the preferred ODB for fetching missing objects.
+ * This should be the alternate with the same directory
+ * name as set in `gvfs.sharedCache`.
+ *
+ * Fallback to .git/objects if necessary.
+ */
 static void gh_client__choose_odb(void)
 {
+	struct object_directory *odb;
+
 	if (gh_client__chosen_odb)
 		return;
 
 	prepare_alt_odb(the_repository);
 	gh_client__chosen_odb = the_repository->objects->odb;
+
+	if (!gvfs_shared_cache_pathname.len)
+		return;
+
+	for (odb = the_repository->objects->odb->next; odb; odb = odb->next) {
+		if (!strcmp(odb->path, gvfs_shared_cache_pathname.buf)) {
+			gh_client__chosen_odb = odb;
+			return;
+		}
+	}
 }
 
 static int gh_client__get(enum gh_client__created *p_ghc)